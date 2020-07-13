64f0986809c71e04a30fd3037ee820819a75de47
ad-1 <andrewdaviesul@gmail.com>
2020-07-13 16:58:41 +0100

    Listing files in dir

--- filesizer/src/Filesizer/Program.cs
+++ filesizer/src/Filesizer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Filesizer
 {
@@ -6,7 +7,13 @@ namespace Filesizer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo d = new DirectoryInfo(@".");
+            FileInfo[] files = d.GetFiles("*.cs");
+            Console.WriteLine("Number of files in directory: " + files.Length);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
         }
     }
 }
