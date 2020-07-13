554a4ddb6a7ba1c69cc0c45c994db9010395e207
Andrew Davies <WZFJCV@EUR.corp.gm.com>
2020-07-13 16:46:09 +0100

    Filesizer app

---
+++ filesizer/src/Filesizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Filesizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
