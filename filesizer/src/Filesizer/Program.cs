c3303e8673d048135b83034bef8f61671db88569
ad-1 <andrewdaviesul@gmail.com>
2020-07-14 10:29:05 +0100

    using linq to simplify filesizer program

--- filesizer/src/Filesizer/Program.cs
+++ filesizer/src/Filesizer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Filesizer
 {
@@ -7,15 +10,49 @@ namespace Filesizer
     {
         static void Main(string[] args)
         {
-            // comment
-            var searchFileExtension = "*.cs";
-            DirectoryInfo d = new DirectoryInfo(@".");
-            FileInfo[] files = d.GetFiles(searchFileExtension);
-            Console.WriteLine("Number of files in directory: " + files.Length);
-            foreach (FileInfo file in files)
+            Console.WriteLine("Full path to search directory: ");
+            var path = Console.ReadLine();
+            var searchFileExtension = "*";
+            GetLargestFiles(path, searchFileExtension);
+            Console.WriteLine("*****");
+            GetLargestFilesUsingLinq(path, searchFileExtension);
+        }
+
+        private static void GetLargestFilesUsingLinq(string path, string searchFileExtension)
+        {
+            var query = from file in new DirectoryInfo(@$"{path}").GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach(var file in query.Take(5))
             {
-                Console.WriteLine(file.Name);
+                Console.WriteLine($"{file.Name,-30 } : {file.Length,10:N0}");
             }
         }
+
+        private static void GetLargestFiles(string path, string searchFileExtension)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@$"{path}");
+            FileInfo[] files = dir.GetFiles(searchFileExtension);
+            if (files.Length < 5)
+            {
+                Console.WriteLine("There are not enough files in this directory for analysis!");
+                return;
+            }
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{files[i].Name,-20 } : {files[i].Length,10:N0}");
+            }
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+
+
     }
 }
